5f72fb4bdb9a5747d43773bc619e202c1ac7ed18
zfitness <zhengmiaode@hotmail.com>
2020-02-17 12:03:57 +0800

    search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" class="search-input" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CitySearch"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background: $bgColor;
+
+  .search-input {
+    height: 0.62rem;
+    width: 100%;
+    text-align: center;
+    border-radius: 0.06rem;
+    line-height: 0.62rem;
+    color: #666;
+    // 
+    box-sizing: border-box;
+    padding: 0 0.2rem;
+  }
+}
+</style>
